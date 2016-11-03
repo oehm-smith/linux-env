2c6db01518b1b6d344e87e0e7e6c0209155ab433
Brooke Smith <brooke.smith@ga.gov.au>
2016-11-03 12:45:11 +1100

    Fixed git-prompt to work properly.  Better organisation.

---
+++ ETC/bashrc.d.direct/git-files.sh
@@ -0,0 +1,4 @@
+# At this stage I'm sourcing each of these directly
+#. /etc/bashrc.d/git-prompt-settings.sh
+#. /etc/bashrc.d/git-prompt.sh
+
---
+++ ETC/profile.d/paths.sh
@@ -0,0 +1,2 @@
+export PATH=/usr/local/bin:$JAVA_HOME/bin:$PATH
+
