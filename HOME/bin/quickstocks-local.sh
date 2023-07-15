7c076e94bddd994418c3aa7b2d278a252c6addbc
Brooke Smith <brooke@oehmsmith.com>
2023-07-15 19:59:19 +1000

    Linux env update 15.7.23
    Highlights:
    * Quickstocks and Weather scripts
    * Terminator in brew

--- HOME/bin/quickstocks-local.sh
+++ HOME/bin/quickstocks-local.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-curl http://localhost:9005/quote/price\?symbols=^axjo,amp.ax,bhp.ax,bkl.ax,bvs.ax,cba.ax,flt.ax,hum.ax,hvn.ax,llc.ax,ori.ax,qbe.ax,s32.ax,sgp.ax,sol.ax,sul.ax,twe.ax,web.ax,wmi.ax,wor.ax\&format=csvPortfolio
+# Run quote for most recent day on stock exchange
+# 15.7.23 - calling script in the project.  A .env file is required and a .env.template should have correct values, but if not it should contain:
+# indices=^axjo
+# symbols=amp.ax,bhp.ax,bkl.ax,bvs.ax,cba.ax,flt.ax,hum.ax,hvn.ax,llc.ax,ori.ax,qbe.ax,s32.ax,sgp.ax,sol.ax,sul.ax,twe.ax,web.ax,wmi.ax,wor.ax
+
+#curl http://localhost:9005/quote/price\?symbols=^axjo,amp.ax,bhp.ax,bkl.ax,bvs.ax,cba.ax,flt.ax,hum.ax,hvn.ax,llc.ax,ori.ax,qbe.ax,s32.ax,sgp.ax,sol.ax,sul.ax,twe.ax,web.ax,wmi.ax,wor.ax\&format=csvPortfolio
+
+pushd ~/dev/home/quickStocks/quickstocks > /dev/null
+. ./scripts/quickStocks-Portfolio-historyToToday.sh
+popd > /dev/null
+
--- HOME/bin/weatherHistory_Canberra_today.sh
+++ HOME/bin/weatherHistory_Canberra_today.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
+echo curl http://localhost:8500/weather?format=cfl\&location=Canberra,ACT
 curl http://localhost:8500/weather?format=cfl\&location=Canberra,ACT
-
