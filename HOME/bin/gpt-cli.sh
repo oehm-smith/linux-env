aa2c3f2998da87771b7cb6d39676a99d147d2065
Brooke Smith <brooke@oehmsmith.com>
2024-03-11 10:28:34 +1100

    Shell script to start GPT CLI

---
+++ HOME/bin/gpt-cli.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Must be running Python 3
+# pyenv - https://github.com/pyenv/pyenv install and setup environment
+#   	pyenv install 3
+#	pyenv version
+#	pyenv global 3.12.1
+
+python /Users/bbos/dev/_thirdParty/gpt4all/gpt4all-bindings/cli/app.py repl
+
