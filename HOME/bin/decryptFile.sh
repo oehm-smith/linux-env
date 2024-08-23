b778f5e807df76df072b2e66d9c02d6ad621a05b
Brooke Smith <brooke@oehmsmith.com>
2024-08-24 08:50:19 +1000

    Changed to allow for files with spaces

--- HOME/bin/decryptFile.sh
+++ HOME/bin/decryptFile.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
-
 pw=$1
 shift
 
-encFiles="$@"
-echo encFiles: $encFiles
+SAVEIFS=$IFS
+IFS=$'\n'
 
-if [ -z "pw" ]; then
+encFiles=("$@")
+echo encFiles: "${encFiles[@]}"
+
+#encFiles="$@"
+#echo encFiles: $encFiles
+
+if [ -z "$pw" ]; then
 	echo USAGE: $0 PW _files Or dirs tar to decrypt_ - missing password
 	exit 1
 fi
 
-if [ -z "$encFiles" ]; then
+if [ -z "${encFiles[@]}" ]; then
 	echo USAGE: $0 PW _files Or dirs to decrypt_
 	exit 1
 fi
@@ -26,17 +31,39 @@ fi
 #	exit 1
 #fi
 
-for encFile in $encFiles
+for encFile in "${encFiles[@]}"
 do
   if [ ! -e "$encFile" ]; then
     echo USAGE: $0 file Or dir to encrypt - aint exist: $encFile
     exit 1
   fi
 
-  name=$(basename "$encFile" .tgz.enc)
+  # I was originally using pipes for this, but if the openssl fails it will still create output file
+  tarName=$(basename "$encFile" .enc)
+  outName=$(basename "$encFile" .tgz.enc)
 
-  echo openssl aes-256-cbc -d -a -pbkdf2  -pass "pass:***" -in "$encFile" PIPE tar xz
-  openssl aes-256-cbc -d -a -pbkdf2 -pass 'pass:$pw' -in "$encFile" | tar xz > "$name"
+  if [ -e "$tarName" ]; then
+    echo Output tar file already exists - aborting: $tarName
+    exit 1
+  fi
+  echo openssl aes-256-cbc -d -a -pbkdf2  -pass "pass:$pw" -in "$encFile" \> "$tarName"
+  openssl aes-256-cbc -d -a -pbkdf2 -pass "pass:$pw" -in "$encFile" > "$tarName"
+
+  if [ $? -ne 0 ]; then
+    echo Error in openssl - aborting - $?
+    rm "$tarName"
+    exit 2
+  fi
+
+  if [ -e "$outName" ]; then
+    echo Output results file already exists - aborting: $outName
+    exit 1
+  fi
+  echo tar xzf "$tarName" \> "$outName"
+  tar xzf "$tarName" > "$outName"
+
+  echo rm "$tarName"
+  rm "$tarName"
 
 #  echo tar cz "$file" pipe openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:***" -out "$file.tgz.enc"
 #  tar cz "$file" | openssl aes-256-cbc -a -salt -pbkdf2 -pass 'pass:$pw' -out "$file.tgz.enc"
--- HOME/bin/encryptFile.sh
+++ HOME/bin/encryptFile.sh
@@ -2,28 +2,33 @@
 pw=$1
 shift
 
-fileOrDirs="$@"
-echo fileOrDirs: $fileOrDirs
+SAVEIFS=$IFS
+IFS=$'\n'
 
-if [ -z "pw" ]; then
+fileOrDirs=("$@")
+echo fileOrDirs: "${fileOrDirs[@]}"
+
+if [ -z "$pw" ]; then
 	echo USAGE: $0 PW _files Or dirs to encrypt_ - missing password
 	exit 1
 fi
 
-if [ -z "$fileOrDirs" ]; then
+if [ -z "${fileOrDirs[@]}" ]; then
 	echo USAGE: $0 PW _files Or dirs to encrypt_
 	exit 1
 fi
 
-for file in $fileOrDirs
+for file in "${fileOrDirs[@]}"
 do
+  echo file: "$file"
   if [ ! -e "$file" ]; then
-    echo USAGE: $0 file Or dir to encrypt
+    echo USAGE: $0 file Or dir to encrypt - not file: "$file"
     exit 1
   fi
 
   echo tar cz "$file" pipe openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:***" -out "$file.tgz.enc"
-  tar cz "$file" | openssl aes-256-cbc -a -salt -pbkdf2 -pass 'pass:$pw' -out "$file.tgz.enc"
+  tar cz "$file" | openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:$pw" -out "$file.tgz.enc"
 done
 
 # Use PW such as encrypt LLM
+IFS=$SAVEIFS
