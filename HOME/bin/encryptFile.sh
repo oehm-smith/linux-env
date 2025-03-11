6a6a815ec5369b98c2e511da607bd54ab090c2f5
Brooke Smith <brooke@oehmsmith.com>
2025-03-12 10:05:40 +1100

    Updated encryptFile

--- HOME/bin/encryptFile.sh
+++ HOME/bin/encryptFile.sh
@@ -15,7 +15,7 @@ fi
 
 if [ -z "${fileOrDirs[@]}" ]; then
 	echo USAGE: $0 PW _files Or dirs to encrypt_
-	exit 1
+	exit 2
 fi
 
 for file in "${fileOrDirs[@]}"
@@ -23,9 +23,13 @@ do
   echo file: "$file"
   if [ ! -e "$file" ]; then
     echo USAGE: $0 file Or dir to encrypt - not file: "$file"
-    exit 1
+    exit 3
   fi
 
+  if [ -e "$file.tgz.enc" ]; then
+    echo Output file already exists - aborting: "$file.tgz.enc"
+    exit 4
+  fi
   echo tar cz "$file" pipe openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:***" -out "$file.tgz.enc"
   tar cz "$file" | openssl aes-256-cbc -a -salt -pbkdf2 -pass "pass:$pw" -out "$file.tgz.enc"
 done
